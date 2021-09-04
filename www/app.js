ca389e53779ceffa4c9d859b497583a05fb0c9e9
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2021-09-04 21:52:52 +0900

    service remastering

--- www/app.js
+++ www/app.js
@@ -11,6 +11,7 @@ const App = new Vue({
         screenshareEnabled: false,
         showIntro: true,
         showChat: false,
+        showYoutubeSet: false,
         showSettings: false,
         hideToolbar: false,
         selectedAudioDeviceId: "",
@@ -46,47 +47,10 @@ const App = new Vue({
         nameToLocalStorage: function() {
             window.localStorage.name = this.name;
         },
-        screenShareToggle: function(e) {
+        youtubeShareToggle: function(e) {
             e.stopPropagation();
-            let screenMediaPromise;
-            if (!App.screenshareEnabled) {
-                if (navigator.getDisplayMedia) {
-                    screenMediaPromise = navigator.getDisplayMedia({ video: true, audio: true });
-                } else if (navigator.mediaDevices.getDisplayMedia) {
-                    screenMediaPromise = navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
-                } else {
-                    screenMediaPromise = navigator.mediaDevices.getUserMedia({
-                        video: { mediaSource: "screen" },
-                    });
-                }
-            } else {
-                screenMediaPromise = navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-            }
-            screenMediaPromise
-                .then((screenStream) => {
-                    App.screenshareEnabled = !App.screenshareEnabled;
+            console.log('youtube');
 
-                    for (let peer_id in peers) {
-                        const sender = peers[peer_id].getSenders().find((s) => (s.track ? s.track.kind === "video" : false));
-                        sender.replaceTrack(screenStream.getVideoTracks()[0]);
-                        sender.replaceTrack(screenStream.getAudioTracks()[0]);
-                    }
-                    screenStream.getVideoTracks()[0].enabled = true;
-                    screenStream.getAudioTracks()[0].enabled = true;
-
-                    const newStream = new MediaStream([screenStream.getVideoTracks()[0], screenStream.getAudioTracks()[0]]);
-                    localMediaStream = newStream;
-                    attachMediaStream(document.getElementById("selfVideo"), newStream);
-                    this.toggleSelfVideoMirror();
-
-                    screenStream.getVideoTracks()[0].onended = function() {
-                        if (App.screenshareEnabled) App.screenShareToggle();
-                    };
-                })
-                .catch((e) => {
-                    alert("화면공유에 실패했어요. 지원되는 브라우저가 아닐 수 있어요.");
-                    console.error(e);
-                });
         },
         changeCamera: function(deviceId) {
             navigator.mediaDevices
@@ -136,15 +100,24 @@ const App = new Vue({
             return safe_tags_replace(str);
         },
         linkify: function(str) {
-            return this.sanitizeString(str).replace(/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%]+/gi, (match) => {
-                let displayURL = match
-                    .trim()
-                    .replace("https://", "")
-                    .replace("https://", "");
-                displayURL = displayURL.length > 25 ? displayURL.substr(0, 25) + "&hellip;" : displayURL;
-                const url = !/^https?:\/\//i.test(match) ? "http://" + match : match;
-                return `<a href="${url}" target="_blank" class="link" rel="noopener">${displayURL}</a>`;
-            });
+            if (str == '/1') {
+                return `<img src="https://blog.kakaocdn.net/dn/VzAMG/btqHR66WyUC/xkuXI9X1b5YGd7Nobr1g00/img.gif" style="width:50%;">`;
+            } else if (str == '/2') {
+                return `<img src="https://cdn.notefolio.net/img/93/c6/93c62f9d432b62cb5f3c96f2094f54f7da8e6313fac02c8b6dffff99fb0ac78c_v1.jpg" style="width:50%;">`;
+            } else if (str == '/3') {
+                return `<img src="https://www.hushwish.com/wp-content/uploads/2019/10/emo_asiana_009.gif" style="width:50%;">`;
+            } else {
+                return this.sanitizeString(str).replace(/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%]+/gi, (match) => {
+                    let displayURL = match
+                        .trim()
+                        .replace("https://", "")
+                        .replace("https://", "");
+                    displayURL = displayURL.length > 25 ? displayURL.substr(0, 25) + "&hellip;" : displayURL;
+                    const url = !/^https?:\/\//i.test(match) ? "http://" + match : match;
+                    return `<a href="${url}" target="_blank" class="link" rel="noopener">${displayURL}</a>`;
+                });
+            }
+
         },
         edit: function(e) {
             this.typing = e.srcElement.textContent;
@@ -167,7 +140,25 @@ const App = new Vue({
                     date: new Date().toISOString(),
                 };
                 this.chats.push(chatMessage);
+                this.$nextTick(() => {
+                    let messages = this.$refs.chats;
+                    chats.scrollTo({ top: chats.scrollHeight, behavior: 'smooth' });
+                });
                 Object.keys(dataChannels).map((peer_id) => dataChannels[peer_id].send(JSON.stringify(chatMessage)));
+                if (this.typing == '/이모티콘') {
+                    const chatMessage = {
+                        type: "chat",
+                        name: "마르크스",
+                        message: '/1 : 윙크\n/2 : 울음\n/3 : 노노\n', //이모티콘 리스트 도움말
+                        date: new Date().toISOString(),
+                    };
+                    this.chats.push(chatMessage);
+                    this.$nextTick(() => {
+                        let messages = this.$refs.chats;
+                        chats.scrollTo({ top: chats.scrollHeight, behavior: 'smooth' });
+                    });
+                    Object.keys(dataChannels).map((peer_id) => dataChannels[peer_id].send(JSON.stringify(chatMessage)));
+                }
                 this.typing = "";
                 composeElement.textContent = "";
                 composeElement.blur;
@@ -178,7 +169,65 @@ const App = new Vue({
                 case "chat":
                     this.showChat = true;
                     this.hideToolbar = false;
+
+                    this.chats.push(chatMessage.split('\n').join('<br>'));
+
+                    this.$nextTick(() => {
+                        let messages = this.$refs.chats;
+                        chats.scrollTo({ top: chats.scrollHeight, behavior: 'smooth' });
+                    });
+                    break;
+                case "youtube":
+                    var player;
+
+                    function onYouTubeIframeAPIReady(id) {
+                        console.log(id);
+                        player = new YT.Player('player', {
+                            width: '100%',
+                            videoId: id,
+                            playerVars: { 'autoplay': 1, 'playsinline': 1 },
+                            events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange }
+                        });
+                    }
+
+                    function onPlayerReady(e) {
+                        e.target.mute();
+                        e.target.playVideo();
+
+                    }
+
+                    // when video ends
+                    function onPlayerStateChange(event) {
+                        if (event.data === 0) {
+                            $('#videos').show();
+                            $('#youtube-sec').hide();
+                            $('.flip-btn').hide();
+                            $('#video-icon-btn').show();
+                            $('#youtube-icon-btn').hide();
+                        }
+                    }
+                    $('#youtubeWrap').hide();
+                    var y_id = (chatMessage.message).replace('Youtube 영상 공유 : ', '');
+                    console.log(y_id);
+                    onYouTubeIframeAPIReady(y_id);
+                    $('#videos').hide();
+                    $('#youtube-sec').show();
+                    $('.flip-btn').show();
+                    $('#video-icon-btn').show();
+                    $('#youtube-icon-btn').hide();
+                    break;
+                case "youtube_shareStop":
+                    $('#player').attr('src', '');
+                    $('#videos').show();
+                    $('#youtube-sec').hide();
+                    $('.flip-btn').hide();
+                    $('#video-icon-btn').show();
+                    $('#youtube-icon-btn').hide();
                     this.chats.push(chatMessage);
+                    this.$nextTick(() => {
+                        let messages = this.$refs.chats;
+                        chats.scrollTo({ top: chats.scrollHeight, behavior: 'smooth' });
+                    });
                     break;
                 default:
                     break;
@@ -194,6 +243,90 @@ const App = new Vue({
                 " " +
                 (date.getHours() >= 12 ? "PM" : "AM")
             );
+        },
+        stopYoutubeShare: function() {
+            $('#player').attr('src', '');
+            //youtube 공유 전달
+            const chatMessage = {
+                type: "youtube_shareStop",
+                name: this.name || "이름없음",
+                message: 'Youtube 영상 공유 종료',
+                date: new Date().toISOString(),
+            };
+            this.chats.push(chatMessage);
+            Object.keys(dataChannels).map((peer_id) => dataChannels[peer_id].send(JSON.stringify(chatMessage)));
+            $('#videos').show();
+            $('#youtube-sec').hide();
+            $('.flip-btn').hide();
+            $('#video-icon-btn').show();
+            $('#youtube-icon-btn').hide();
+        },
+        youtubeBtn: function(e) {
+            var player;
+
+            function onYouTubeIframeAPIReady(id) {
+                console.log(id);
+                player = new YT.Player('player', {
+                    width: '100%',
+                    videoId: id,
+                    playerVars: { 'autoplay': 1, 'playsinline': 1 },
+                    events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange }
+                });
+            }
+
+            function onPlayerReady(e) {
+                e.target.mute();
+                e.target.playVideo();
+
+            }
+
+            // when video ends
+            function onPlayerStateChange(event) {
+                if (event.data === 0) {
+                    $('#videos').show();
+                    $('#youtube-sec').hide();
+                    $('.flip-btn').hide();
+                    $('#video-icon-btn').show();
+                    $('#youtube-icon-btn').hide();
+                }
+            }
+
+            function youtubeId(url) {
+                var tag = "";
+                if (url) {
+                    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+                    var matchs = url.match(regExp);
+                    if (matchs) {
+                        tag += matchs[7];
+                    }
+                    return tag;
+                }
+            }
+            e.stopPropagation();
+            e.preventDefault();
+            var input = $('#youtube-input').val();
+            if (input.length) {
+                var y_id = youtubeId(input);
+                if (input) {
+                    $('#youtubeWrap').hide();
+                    onYouTubeIframeAPIReady(y_id);
+                    $('#videos').hide();
+                    $('#youtube-sec').show();
+                    $('.flip-btn').show();
+                    $('#video-icon-btn').show();
+                    $('#youtube-icon-btn').hide();
+
+                    //youtube 공유 전달
+                    const chatMessage = {
+                        type: "youtube",
+                        name: this.name || "이름없음",
+                        message: 'Youtube 영상 공유 : ' + y_id,
+                        date: new Date().toISOString(),
+                    };
+                    this.chats.push(chatMessage);
+                    Object.keys(dataChannels).map((peer_id) => dataChannels[peer_id].send(JSON.stringify(chatMessage)));
+                }
+            }
         },
     },
 });
